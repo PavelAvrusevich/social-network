f2d02095913faf0934c65360b0596ea1857162f8
PavelAvrusevich <p.avrusevich@gmail.com>
2020-11-10 06:22:20 +0300

    commit 3

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,38 +1,41 @@
-import { NavLink } from "react-router-dom";
-import s from "./Dialogs.module.css";
+import { NavLink } from 'react-router-dom';
+import s from './Dialogs.module.css';
 
-function DialogItem (props) {
-  return (
-    <div className={s.dialog + " " + s.active}>
-       <NavLink to={'/dialogs/'+props.id}>{props.name}</NavLink> 
-    </div>
-  )
+function DialogItem(props) {
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+        </div>
+    );
 }
 
-function Message (props) {
-  return (
-    <div className={s.message}>{props.message}</div>
-  )
+function Message(props) {
+    return <div className={s.message}>{props.message}</div>;
 }
 
 function Dialogs() {
-  return (
-    <div className={s.dialogs}>
-      <div className={s.dialogsItems}>
-        <DialogItem name='Серега' id='1' />
-        <DialogItem name='Вован' id='2' />
-        <DialogItem name='Паша' id='3' />
-        <DialogItem name='Женя' id='4' />
-        <DialogItem name='Саня' id='5' />
-      </div>
-      <div className={s.messages}>
-        <Message message="message 1" />
-        <Message message="message 2" />
-        <Message message="message 3" />
-        <Message message="message 4" />
-      </div>
-    </div>
-  );
+    let dialogsData = [
+        { id: '1', name: 'Серега' },
+        { id: '2', name: 'Вован' },
+    ];
+
+    let messagesData = [
+        { id: '1', message: 'message 1' },
+        { id: '2', message: 'message 2' },
+    ];
+
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+            </div>
+            <div className={s.messages}>
+                <Message message={messagesData[0].message} />
+                <Message message={messagesData[1].message} />
+            </div>
+        </div>
+    );
 }
 
 export default Dialogs;
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,26 +1,37 @@
-import s from "./MyPosts.module.css";
-import Post from "./Post/Post";
+import s from './MyPosts.module.css';
+import Post from './Post/Post';
 
 function MyPosts() {
-  return (
-    <div className={s.postsBlock}>
-      <h2>MyPosts</h2>
-      <div>
-        <textarea></textarea>
-      </div>
-      <div>
-        <button>Add post</button>
-      </div>
-      <div>
-        my post
-        <div>new post</div>
-        <div className={s.posts}>
-          <Post message="привет, как дела??" />
-          <Post message="Я ждун))" />
+    let postsData = [
+        { id: '1', message: 'привет, как дела??', likesCount: '12' },
+        { id: '2', message: 'Я ждун))', likesCount: '10' },
+    ];
+
+    return (
+        <div className={s.postsBlock}>
+            <h2>MyPosts</h2>
+            <div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button>Add post</button>
+            </div>
+            <div>
+                my post
+                <div>new post</div>
+                <div className={s.posts}>
+                    <Post
+                        message={postsData[0].message}
+                        likesCount={postsData[0].likesCount}
+                    />
+                    <Post
+                        message={postsData[1].message}
+                        likesCount={postsData[1].likesCount}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default MyPosts;
