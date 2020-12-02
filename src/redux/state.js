d593e87bb1a221cb9d7215d76fb128486681287d
PavelAvrusevich <p.avrusevich@gmail.com>
2020-12-02 19:58:04 +0300

    create state

---
+++ src/redux/state.js
@@ -0,0 +1,32 @@
+let state = {
+    profilePage: {
+        posts: [
+            { id: '1', message: 'привет, как дела??', likesCount: '12' },
+            { id: '2', message: 'Я ждун))', likesCount: '10' },
+        ],
+    },
+    dialogsPage: {
+        dialogs: [
+            { id: '1', name: 'Серега' },
+            { id: '2', name: 'Вован' },
+            { id: '3', name: 'Силос' },
+        ],
+        messages: [
+            { id: '1', message: 'message 1' },
+            { id: '2', message: 'message 2' },
+            { id: '3', message: 'message 121' },
+        ],
+        sidebar: {
+            friends: [
+                { id: '1', name: 'Сергей' },
+                { id: '2', name: 'Александр' },
+                { id: '3', name: 'Дмитрий' },
+                { id: '4', name: 'Кристина' },
+                { id: '5', name: 'Егор' },
+                { id: '6', name: 'Тимур' },
+            ],
+        },
+    },
+};
+
+export default state;
