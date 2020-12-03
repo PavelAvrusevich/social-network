2d186d449802848b811e2abe4a737513e322e67e
PavelAvrusevich <p.avrusevich@gmail.com>
2020-12-03 23:39:24 +0300

    chc

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import state from './redux/state';
+import store from './redux/state';
 import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost, fixNewText, subscriber } from './redux/state';
 
 let rerender = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} fixNewText={fixNewText} />
+            <App
+                state={state}
+                addPost={store.addPost.bind(store)}
+                fixNewText={store.fixNewText.bind(store)}
+            />
         </BrowserRouter>,
         document.getElementById('root')
     );
 };
 
-rerender(state);
-subscriber(rerender);
+rerender(store.getState());
+
+store.subscribe(rerender);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,56 +1,56 @@
-let rerender;
-
-let state = {
-    profilePage: {
-        posts: [
-            { id: '1', message: 'привет, как дела??', likesCount: 12 },
-            { id: '2', message: 'Я ждун))', likesCount: 10 },
-        ],
-        newText: '',
-    },
-    dialogsPage: {
-        dialogs: [
-            { id: '1', name: 'Серега' },
-            { id: '2', name: 'Вован' },
-            { id: '3', name: 'Силос' },
-        ],
-        messages: [
-            { id: '1', message: 'message 1' },
-            { id: '2', message: 'message 2' },
-            { id: '3', message: 'message 121' },
-        ],
-        sidebar: {
-            friends: [
-                { id: '1', name: 'Сергей' },
-                { id: '2', name: 'Александр' },
-                { id: '3', name: 'Дмитрий' },
-                { id: '4', name: 'Кристина' },
-                { id: '5', name: 'Егор' },
-                { id: '6', name: 'Тимур' },
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: '1', message: 'привет, как дела??', likesCount: 12 },
+                { id: '2', message: 'Я ждун))', likesCount: 10 },
             ],
+            newText: '',
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: '1', name: 'Серега' },
+                { id: '2', name: 'Вован' },
+                { id: '3', name: 'Силос' },
+            ],
+            messages: [
+                { id: '1', message: 'message 1' },
+                { id: '2', message: 'message 2' },
+                { id: '3', message: 'message 121' },
+            ],
+            sidebar: {
+                friends: [
+                    { id: '1', name: 'Сергей' },
+                    { id: '2', name: 'Александр' },
+                    { id: '3', name: 'Дмитрий' },
+                    { id: '4', name: 'Кристина' },
+                    { id: '5', name: 'Егор' },
+                    { id: '6', name: 'Тимур' },
+                ],
+            },
         },
     },
+    getState() {
+        return this._state;
+    },
+    addPost() {
+        let newPost = {
+            id: 3,
+            message: this._state.profilePage.newText,
+            likesCount: 0,
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newText = '';
+        this._callSubscriber(this._state);
+    },
+    fixNewText(changeText) {
+        this._state.profilePage.newText = changeText;
+        this._callSubscriber(this._state);
+    },
+    _callSubscriber() {},
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 };
 
-let addPost = () => {
-    let newPost = {
-        id: 3,
-        message: state.profilePage.newText,
-        likesCount: 0,
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newText = '';
-    rerender(state);
-};
-
-let fixNewText = (changeText) => {
-    state.profilePage.newText = changeText;
-    rerender(state);
-};
-
-const subscriber = (observer) => {
-    rerender = observer;
-};
-
-export { addPost, fixNewText, subscriber };
-export default state;
+export default store;
